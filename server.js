d1b8dde9e62bb7328ebe45796ecbb83e1d999160
Bety <betyrmartinez@gmail.com>
2022-07-30 14:50:48 -0300

    prueba2

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors({
-    origin: 'https://app-front-proyecto.herokuapp.com/' //Dirección del front
+    origin: 'https://app-front-proyecto.herokuapp.com' //Dirección del front
 }))
 
 
